47f05c66af4bfda3577323e15acdfff6182eb1c3
berenfeld <berenfeldran@gmail.com>
2021-10-02 19:07:01 +0000

    update construction unit tests

--- tests/test_cint.cpp
+++ tests/test_cint.cpp
@@ -5,9 +5,12 @@ TEST(UInt, Constructions)
 {
     large_numbers::UInt zero;
     large_numbers::UInt one("1", 16);
+    large_numbers::UInt one2(1);
+    EXPECT_EQ(one, one2);
+
     large_numbers::UInt two = one * 2;
-    large_numbers::UInt another_two("2", 16);
-    EXPECT_EQ(two, another_two);
+    large_numbers::UInt two2("2", 16);
+    EXPECT_EQ(two, two2);
 }
 
 TEST(UInt, Base16String)
