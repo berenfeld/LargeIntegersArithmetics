e4196ff1956f49458d1efd897aa4fc42d2994813
berenfeld <berenfeldran@gmail.com>
2021-10-03 18:24:58 +0000

    Multiple fixes and support base 10 parsing

--- include/uint.h
+++ include/uint.h
@@ -1,5 +1,8 @@
+#pragma once
+
 #include <string>
 #include <vector>
+#include <functional>
 
 namespace large_numbers
 {
@@ -11,11 +14,28 @@ namespace large_numbers
         UInt(const std::string& str, int base = 10);
 
         bool operator ==(const UInt& other) const;
+        UInt& operator +=(const UInt& other);
 
         UInt operator *(uint32_t arg) const;
 
         std::string toString(int base = 10) const;
+
+        class UIntHash
+        {
+            public:
+            size_t operator()(const UInt& key) const
+            {
+                size_t result = 0;
+                for (const uint32_t& value : key._values)
+                {
+                    result += std::hash<uint32_t>()(value);
+                }
+                return result;
+            }
+        };
     private:
         std::vector<uint32_t> _values;
     };
+
+
 }
--- src/uint.cpp
+++ src/uint.cpp
@@ -3,6 +3,8 @@
 #include "utils.h"
 #include <sstream>
 #include <algorithm>
+#include <iomanip>
+#include <iostream>
 
 namespace large_numbers
 {
@@ -18,6 +20,11 @@ namespace large_numbers
     {
         switch(base)
         {
+            case 10: {
+                UInt result = parseBase10StringValues(str);
+                _values = result._values;
+                break;
+            }
             case 16: {
                 parseBase16StringValues(str, _values);
                 break;
@@ -30,6 +37,22 @@ namespace large_numbers
     bool UInt::operator ==(const UInt& other) const
     {
         return _values == other._values;
+    }
+
+    UInt& UInt::operator +=(const UInt& other)
+    {
+        _values.resize(std::max(_values.size(), other._values.size()));
+        uint32_t add_to_next_value = 0;
+        for(int i=0; i<other._values.size(); ++i)
+        {
+            uint64_t added_value = static_cast<uint64_t>(_values[i]) + static_cast<uint64_t>(other._values[i]) + add_to_next_value;
+            _values[i] = added_value;
+            add_to_next_value = added_value >> 32;
+        }
+        if (add_to_next_value) {
+            _values.push_back(add_to_next_value);
+        }
+        return *this;
     }
 
     UInt UInt::operator *(uint32_t arg) const
@@ -55,8 +78,12 @@ namespace large_numbers
         {
             case 16: {
                 std::stringstream ss;
-                for (const uint32_t& value : _values) {
-                    ss << std::hex << value;
+                for (int i=_values.size()-1;i>=0;--i) {
+                    ss << std::hex;
+                    if (i != _values.size() - 1) {
+                        ss << std::setfill('0') << std::setw(8);
+                    }  
+                    ss << _values[i];
                 }
                 std::string result = ss.str();
                 std::transform(result.begin(), result.end(),result.begin(), ::toupper);
