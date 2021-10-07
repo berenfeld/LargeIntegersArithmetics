4589e4671ad010d209c03cdeac834cd222a7d1cd
berenfeld <berenfeldran@gmail.com>
2021-10-07 12:55:05 +0000

    auto format and enforce with clang-format

--- include/uint.h
+++ include/uint.h
@@ -1,72 +1,76 @@
 #pragma once
 
+#include <functional>
 #include <string>
 #include <vector>
-#include <functional>
 
 namespace large_numbers
 {
-    class UInt
+class UInt
+{
+  public:
+    UInt();
+    UInt(uint32_t value);
+    UInt(const std::string &str, int base = 0);
+
+    // comparison
+    bool operator==(const UInt &other) const;
+    bool operator==(uint32_t value) const;
+    bool operator!=(const UInt &other) const;
+    bool operator!=(uint32_t value) const;
+
+    // addition
+    UInt &operator+=(const UInt &other);
+    UInt operator+(const UInt &other) const;
+    UInt &operator+=(uint32_t arg);
+    UInt operator+(uint32_t arg);
+
+    // subsctraction
+    UInt &operator-=(const UInt &other);
+    UInt operator-(const UInt &other) const;
+
+    // multiplication
+    UInt operator*(uint32_t arg) const;
+
+    // shift
+    UInt operator<<(uint32_t offset) const;
+
+    [[nodiscard]] size_t size() const;
+    uint32_t getBlock(int i) const;
+
+    [[nodiscard]] std::string toString(int base = 10) const;
+
+    friend std::ostream &operator<<(std::ostream &os, const UInt &value)
     {
-    public:
-        UInt();
-        UInt(uint32_t value);
-        UInt(const std::string& str, int base = 0);
+        return os << value.toString(16);
+    }
 
-        // comparison
-        bool operator ==(const UInt& other) const;
-        bool operator ==(uint32_t value) const;
-        bool operator !=(const UInt& other) const;
-        bool operator !=(uint32_t value) const;
-
-        // addition
-        UInt& operator +=(const UInt& other);
-        UInt operator +(const UInt& other) const;
-        UInt& operator +=(uint32_t arg);
-        UInt operator +(uint32_t arg);
-
-        // subsctraction
-        UInt& operator -=(const UInt& other);
-        UInt operator -(const UInt& other) const;
-
-        // multiplication
-        UInt operator *(uint32_t arg) const;
-
-        // shift
-        UInt operator <<(uint32_t offset) const;
-
-        [[nodiscard]] size_t size() const;
-        uint32_t getBlock(int i) const;
-
-        [[nodiscard]] std::string toString(int base = 10) const;
-
-        friend std::ostream& operator<<(std::ostream& os, const UInt& value) {
-            return os << value.toString(16);
-        }
-
-        class UIntHash
+    class UIntHash
+    {
+      public:
+        size_t operator()(const UInt &key) const
         {
-            public:
-            size_t operator()(const UInt& key) const
+            size_t result = 0;
+            for (const uint32_t &value : key._values)
             {
-                size_t result = 0;
-                for (const uint32_t& value : key._values)
-                {
-                    result += std::hash<uint32_t>()(value);
-                }
-                return result;
+                result += std::hash<uint32_t>()(value);
             }
-        };
-    private:
-        /**
-         * @brief negate all the bits in the number, and after negation trim leading zeros
-         * note : unlike fixed size ~ operator this operator does not reverse. i.e sometimes negate(negate(x)) != x
-         * TODO : formalize it, using either fixed block size argument or storing of "negate back" block size
-         * @return UInt 
-         */
-        [[nodiscard]] UInt negate() const;
-
-        std::vector<uint32_t> _values;
+            return result;
+        }
     };
 
-}
+  private:
+    /**
+     * @brief negate all the bits in the number, and after negation trim leading
+     * zeros note : unlike fixed size ~ operator this operator does not reverse.
+     * i.e sometimes negate(negate(x)) != x
+     * TODO : formalize it, using either fixed block size argument or storing of
+     * "negate back" block size
+     * @return UInt
+     */
+    [[nodiscard]] UInt negate() const;
+
+    std::vector<uint32_t> _values;
+};
+
+} // namespace large_numbers
