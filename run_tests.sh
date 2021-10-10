64c9204f512e84b3cd428d613de964e396195430
berenfeld <berenfeldran@gmail.com>
2021-10-10 13:20:34 +0000

    separate Debug and Release flows

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,10 @@
 ./bin/bash
 
-mkdir -p build/coverage/html
-./build/TestLargeInts $@
-gcovr -s -r . --html --html-details -o build/coverage/html/index.html
+if [[ -z $BUILD_TYPE ]]; then
+    BUILD_TYPE=Debug
+fi
+echo Running tests with BUILD_TYPE=$BUILD_TYPE
+
+mkdir -p build/$BUILD_TYPE/coverage/html
+./build/$BUILD_TYPE/TestLargeInts $@
+gcovr -s -r . --html --html-details -o build/$BUILD_TYPE/coverage/html/index.html
