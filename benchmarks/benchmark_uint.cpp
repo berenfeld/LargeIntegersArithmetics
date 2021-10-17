8a21533a92338e9aa06d38866fab8b375b66c500
berenfeld <berenfeldran@gmail.com>
2021-10-17 18:10:30 +0000

    add benchmarks docs

--- benchmarks/benchmark_uint.cpp
+++ benchmarks/benchmark_uint.cpp
@@ -1,12 +1,13 @@
-#include "uint.h"
-#include "utils.h"
 #include "consts.h"
 #include "primes_base.h"
+#include "uint.h"
+#include "utils.h"
 #include <benchmark/benchmark.h>
 
 using namespace large_numbers;
 
-void addition(benchmark::State& state) {
+void addition(benchmark::State &state)
+{
     for (auto _ : state) {
         UInt a = large_numbers::rand(LN_BENCHMARK_UINT_BITS / LN_BITS_IN_BLOCK);
         UInt b = large_numbers::rand(LN_BENCHMARK_UINT_BITS / LN_BITS_IN_BLOCK);
@@ -14,7 +15,8 @@ void addition(benchmark::State& state) {
     }
 }
 
-void multiplication(benchmark::State& state) {
+void multiplication(benchmark::State &state)
+{
     for (auto _ : state) {
         UInt a = large_numbers::rand(LN_BENCHMARK_UINT_BITS / LN_BITS_IN_BLOCK);
         UInt b = large_numbers::rand(LN_BENCHMARK_UINT_BITS / LN_BITS_IN_BLOCK);
@@ -22,36 +24,27 @@ void multiplication(benchmark::State& state) {
     }
 }
 
-void gcd(benchmark::State& state) {
+void power(benchmark::State &state)
+{
+    for (auto _ : state) {
+        UInt base = large_numbers::rand(LN_BENCHMARK_UINT_BITS / LN_BITS_IN_BLOCK);
+        auto exp = ::rand() % 10;
+        UInt c = UInt::pow(base, exp);
+    }
+}
+
+void gcd(benchmark::State &state)
+{
     for (auto _ : state) {
         UInt a = large_numbers::rand(LN_BENCHMARK_UINT_BITS / LN_BITS_IN_BLOCK);
         UInt b = large_numbers::rand(LN_BENCHMARK_UINT_BITS / LN_BITS_IN_BLOCK);
-        UInt c = UInt::gcd(a,b);
+        UInt c = UInt::gcd(a, b);
     }
 }
-
-
-void searchAroundRSA100(benchmark::State& state) {
-    for (auto _ : state) {
-        UInt rsa_100(
-        "1522605027922533360535618378132637429718068114961380688657908494580122963258952897654000350692006139");
-        UInt z = rsa_100.sqrt() += 1;
-        PrimesBase base(128);
-        for (int i = 0; i < 10; ++i) {
-            UInt candidate = UInt::power_modulo(z, 2, rsa_100);
-            if (base.contains(candidate)) {
-                z += 1;
-                continue;
-            }
-            z += 1;
-        }
-    }
-}
-
 
 BENCHMARK(addition);
 BENCHMARK(multiplication);
-BENCHMARK(searchAroundRSA100);
+BENCHMARK(power);
 BENCHMARK(gcd);
 
 BENCHMARK_MAIN();
