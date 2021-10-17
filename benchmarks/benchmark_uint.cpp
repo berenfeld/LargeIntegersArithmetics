55c5243b220ae4f078585e4bb824d4e7cdfcd52c
berenfeld <berenfeldran@gmail.com>
2021-10-17 12:33:25 +0000

    add search around rsa 100 to benchmark

--- benchmarks/benchmark_uint.cpp
+++ benchmarks/benchmark_uint.cpp
@@ -1,6 +1,7 @@
 #include "uint.h"
 #include "utils.h"
 #include "consts.h"
+#include "primes_base.h"
 #include <benchmark/benchmark.h>
 
 using namespace large_numbers;
@@ -30,8 +31,27 @@ void gcd(benchmark::State& state) {
 }
 
 
+void searchAroundRSA100(benchmark::State& state) {
+    for (auto _ : state) {
+        UInt rsa_100(
+        "1522605027922533360535618378132637429718068114961380688657908494580122963258952897654000350692006139");
+        UInt z = rsa_100.sqrt() += 1;
+        PrimesBase base(128);
+        for (int i = 0; i < 10; ++i) {
+            UInt candidate = UInt::power_modulo(z, 2, rsa_100);
+            if (base.contains(candidate)) {
+                z += 1;
+                continue;
+            }
+            z += 1;
+        }
+    }
+}
+
+
 BENCHMARK(addition);
 BENCHMARK(multiplication);
+BENCHMARK(searchAroundRSA100);
 BENCHMARK(gcd);
 
 BENCHMARK_MAIN();
