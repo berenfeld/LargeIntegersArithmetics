bfcf6bd0fdaed9e2f291f8d394a0dfe46dc36dfc
Ran Berenfeld <berenfeldran@gmail.com>
2021-10-20 10:11:38 +0300

    base10 cache auto extending

--- src/base10_cache.cpp
+++ src/base10_cache.cpp
@@ -4,18 +4,29 @@
 
 namespace large_numbers
 {
-    Base10Cache::Base10Cache()
+    Base10Cache::Base10Cache() : max_digit_pos_cached(0) {}
+
+    void Base10Cache::extendTo(size_t max_digit_pos)
     {
-        int digit_pos = 0;
-        UInt base10_num(1);
-        for (; digit_pos < MAX_DECIMAL_DIGITS; ++digit_pos, base10_num = base10_num * 10) {
+        UInt base10_num = UInt::pow(10, max_digit_pos_cached);
+        for (size_t digit_pos = max_digit_pos_cached; digit_pos < max_digit_pos;
+             ++digit_pos, base10_num = base10_num * 10) {
             for (int digit_value = 0; digit_value < 10; ++digit_value) {
                 _cache[makeKey(digit_pos, digit_value)] = base10_num * digit_value;
             }
         }
+        max_digit_pos_cached = max_digit_pos;
     }
 
-    UInt Base10Cache::get(size_t digit_pos, size_t digit_value) { return _cache.at(makeKey(digit_pos, digit_value)); }
+    UInt Base10Cache::get(size_t digit_pos, size_t digit_value)
+    {
+        auto found = _cache.find(makeKey(digit_pos, digit_value));
+        if (found == _cache.end()) {
+            extendTo(digit_pos + 1);
+            found = _cache.find(makeKey(digit_pos, digit_value));
+        }
+        return found->second;
+    }
 
     size_t Base10Cache::makeKey(size_t digit_pos, size_t digit_value) { return digit_pos << 16 | digit_value; }
     Base10Cache base10Cache;
--- src/base2_cache.cpp
+++ src/base2_cache.cpp
@@ -8,7 +8,7 @@ namespace large_numbers
 
     void Base2Cache::extendTo(size_t max_digit_pos)
     {
-        UInt base2_num = 1 << max_digit_pos_cached;
+        UInt base2_num = UInt(1) << max_digit_pos_cached;
         for (size_t digit_pos = max_digit_pos_cached; digit_pos < max_digit_pos;
              ++digit_pos, base2_num = base2_num << 1) {
             _cache[digit_pos] = base2_num;
@@ -20,7 +20,7 @@ namespace large_numbers
     {
         auto found = _cache.find(digit_pos);
         if (found == _cache.end()) {
-            extendTo(digit_pos);
+            extendTo(digit_pos + 1);
             found = _cache.find(digit_pos);
         }
         return found->second;
